64f53a27ad3da78a49ea0bd2bcf73edb14d9e5bc
sunandarradit3-maker <sunandarradit3@gmail.com>
2026-04-25 01:36:44 +0700

    Create loading.tsx

---
+++ app/products/[id]/loading.tsx
@@ -0,0 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
+export default function Loading() {
+  return <div className="container-page py-10"><Skeleton className="h-[520px] rounded-3xl shimmer" /></div>;
+}
